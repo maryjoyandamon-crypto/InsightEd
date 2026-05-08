c066dd5b5e3594be9a06c079ef8a9c7cbcc5983d
maryjoyandamon-crypto <maryjoy.andamon@bisu.edu.ph>
2026-05-08 19:07:53 +0800

    Add files via upload

--- connection.php
+++ connection.php
@@ -1,13 +1,12 @@
 <?php
-$servername = "localhost";
-$username = "admin";
-$password = "@dm1n_061313";
-$dbname = "insighted";
+$servername = "sql202.infinityfree.com";
+$username = "if0_41864127";
+$password = "ANG_IMONG_PASSWORD"; 
+$dbname = "if0_41864127_insighted_db";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
-   die("Connection failed: " . $conn->connect_error);
+    die("Connection failed: " . $conn->connect_error);
 }
-//echo "Connected successfully";
 ?>
